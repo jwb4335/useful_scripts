debb5d243dd07930dd2323ac696cf7a6713d93b3
jwb4335 <51172981+jwb4335@users.noreply.github.com>
2021-05-28 12:51:29 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='useful_scripts',
+   version='1.0',
+   description='Some useful scripts',
+   author='jwb4335',
+   packages=['to_tex','to_excel','winsor']  #same as name
+)
